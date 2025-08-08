72155d23a70576586484e28c9ae9e5d7d98d0c4a
BenKeng <ben.bamberger@me.com>
2025-08-08 14:45:53 +0000

    Information about calc status

---
+++ calc.py
@@ -0,0 +1,26 @@
+print('Calculator ready for input!')
+str1=input('Enter first number: ')
+try:
+    num1=int(str1)
+except:
+    print('Invalid input')
+action=input('Enter action (+, -, *, /): ')
+if action not in ['+', '-', '*', '/']:
+    print('Invalid input')
+str2=input('Enter second number: ')
+try:
+    num2=int(str2)
+except:
+    print('Invalid input')
+if action == '+':
+    print(num1 + num2)
+elif action == '-':
+    print(num1 - num2)
+elif action == '*':
+    print(num1 * num2)
+elif action == '/':
+    if num2 == 0:
+        print('Division by zero is not possible')
+    else:
+        print(num1 / num2)
+        
